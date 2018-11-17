849c2f5f45cc0e5eff78f93434ea301c4b4d1f8b
Juan Bolívar <juan@juans-macbook-pro.local>
2018-11-17 12:50:00 -0500

    Initial Commit

---
+++ TabataTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TabataTimer
+//
+//  Created by Juan Bolívar on 11/17/18.
+//  Copyright © 2018 Oneware.co. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
