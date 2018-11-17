2e04e2ed089ef7a673609661a9e0e7229d902d02
Juan Bolívar <juan@juans-macbook-pro.local>
2018-11-17 18:13:34 -0500

    UI

--- TabataTimer/ViewController.swift
+++ TabataTimer/ViewController.swift
@@ -9,7 +9,11 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var minuteField: NSTextField!
+    @IBOutlet weak var secondField: NSTextField!
+    @IBOutlet weak var intervalLabel: NSTextField!
+    @IBOutlet weak var totalLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +26,26 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func startButton(_ sender: Any) {
+        let minutes = minuteField.integerValue
+        let seconds = secondField.integerValue
+        if (minutes>=0 && seconds>0) || (minutes>0 && seconds>=0){
+            let interval = "\(minutes):\(seconds)"
+            intervalLabel.stringValue = interval
+            // change start by pause
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        clearText(field: minuteField)
+        clearText(field: secondField)
+        clearText(field: intervalLabel)
+        clearText(field: totalLabel)
+        
+    }
+    
+    func clearText(field: NSTextField){
+        field.stringValue = ""
+    }
 }
 
