072d8717f54e303409e313938ecc3a211e8d3df5
Juan Bolívar <juan@juans-macbook-pro.local>
2018-11-17 19:37:15 -0500

    start button

--- TabataTimer/ViewController.swift
+++ TabataTimer/ViewController.swift
@@ -13,27 +13,40 @@ class ViewController: NSViewController {
     @IBOutlet weak var secondField: NSTextField!
     @IBOutlet weak var intervalLabel: NSTextField!
     @IBOutlet weak var totalLabel: NSTextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
+    @IBOutlet weak var startButton: NSButton!
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
 
     @IBAction func startButton(_ sender: Any) {
-        let minutes = minuteField.integerValue
-        let seconds = secondField.integerValue
-        if (minutes>=0 && seconds>0) || (minutes>0 && seconds>=0){
-            let interval = "\(minutes):\(seconds)"
-            intervalLabel.stringValue = interval
-            // change start by pause
+        if(startButton.title == "Start"){
+            let minutes = minuteField.integerValue
+            let seconds = secondField.integerValue
+            //readonly fields
+            if (minutes>=0 && seconds>0) || (minutes>0 && seconds>=0){
+                //let interval = "\(minutes):\(seconds)"
+                //intervalLabel.stringValue = interval
+                runTimer(intervals: 1)
+                startButton.title = "Pause"
+            }
         }
+        else if (startButton.title == "Pause"){
+            timer.invalidate()
+            startButton.title = "Resume"
+        }
+        else{
+            runTimer(intervals: 1)
+            startButton.title = "Pause"
+        }
+    }
+    
+    func runTimer(intervals: int_fast8_t) {
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(intervals), target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        totalLabel.stringValue = "\(seconds)" //This will update the label.
     }
     
     @IBAction func resetButton(_ sender: Any) {
@@ -41,11 +54,22 @@ class ViewController: NSViewController {
         clearText(field: secondField)
         clearText(field: intervalLabel)
         clearText(field: totalLabel)
-        
+        startButton.title = "Start"
     }
     
     func clearText(field: NSTextField){
         field.stringValue = ""
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
     }
 }
 
